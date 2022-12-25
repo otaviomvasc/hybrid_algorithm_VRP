0918f142980ef6171da3eb11365a9347e222a812
otaviomvasc <otavio_vasconcelos@outlook.com>
2022-12-25 18:09:58 -0300

    corrections to improve the convergion

--- PSO.py
+++ PSO.py
@@ -12,11 +12,10 @@ class Individuo(object):
         return object.__new__(cls)
 
     def __init__(self, new_ind=None, size=None, change_vel=None):
-        if new_ind is None:
-            ind = list(range(1, size))
-            position = [random.uniform(-1,1) for i in range(1,size)]
+            ind = new_ind
+            position = [random.uniform(-1,1) for i in range(len(ind))]
             self.order_per_position(ind, position)
-            self.velocity = np.array([random.uniform(-4.0,4.0) for i in range(len(self.ind))])
+            self.velocity = np.array([random.uniform(-2,2) for i in range(len(self.ind))])
             routes = commons.define_routes(vetor=self.ind, demands=demands, vehicle_capacity=vehicle_capacity)
             self.fit = commons.dist_calculate(vet_aux=routes, matrix=matrix)
             self.best_local = deepcopy(self)
@@ -40,17 +39,18 @@ class Individuo(object):
             indices = list(range(len(self.ind)))
             indices.sort(key=lambda i: self.position[i])
             self.position = np.array([self.position[i] for i in indices])
-            self.ind = [self.ind[i] for i in indices]
+            self.ind = [nodes[i] for i in indices]
+
 
     def fitness(self):
         return self.fit
 
-    def velocity_calculation(self, best, c1r1, c2r2):
-        self.velocity = self.velocity + c1r1 * (self.best_local.position - self.position) + c2r2 * (best.position - self.position)
+    def velocity_calculation(self, best, c1, c2,r1,r2):
+        self.velocity = self.velocity + c1  * r1 * (self.best_local.position - self.position) + c2 * r2* (best.position - self.position)
 
 
 if __name__ == "__main__":
-    path = "instances/eil23.vrp.txt"
+    path = "instances/eilb101.vrp.txt"
 
     problem = ReadTSPLIB(path=path)
     vehicle_capacity = problem.get_vehicle_capacites()
@@ -58,27 +58,29 @@ if __name__ == "__main__":
     matrix = problem.get_matrix()
     demands = problem.get_demands()
     coords = problem.get_coords()
-    nodes = problem.get_node_list()
+    nodes = problem.get_node_list()[1:]
     pop = 500
-    iterations = 400
+    iterations = 500
     i = 0
-    c1r1 = 1.2
-    c2r2 = 1.8
+    c1 = 1.4
+    c2 = 1.4
 
 
-    population = [Individuo(size=len(nodes)) for i in range(pop)]
+    population = [Individuo(new_ind=nodes) for i in range(pop)]
     g_best = min([p.fitness() for p in population])
     best = [p for p in population if p.fitness() == g_best][0]
 
     while i < iterations:
+        r1 = random.uniform(0,1)
+        r2 = random.uniform(0,1)
         #atualizar o position, fit e best_local
         for ind in population:
-            ind.velocity_calculation(best, c1r1, c2r2)
+            ind.velocity_calculation(best, c1, c2, r1, r2)
             ind.position = ind.position + ind.velocity
             ind.order_per_position()
             ind.fitness_calculation(demands, vehicle_capacity, matrix)
             ind.best_local_actualize()
-
+            #ind.velocity_calculation(best, c1r1, c2r2)
 
         g_best_candidate = min([p.fitness() for p in population])
         best_candidate = [p for p in population if p.fitness() == g_best_candidate][0]
